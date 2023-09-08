112310e1734bf92fee322b70810326e74a37e630
TikhonS1r <litmanlow@gmail.com>
2023-09-08 10:51:20 +0300

    hello

--- SYROMYATNIKOV_AA2208/SYROMYATNIKOV_AA2208/Syromyatnikov.cpp
+++ SYROMYATNIKOV_AA2208/SYROMYATNIKOV_AA2208/Syromyatnikov.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Hello World!";
+    std:: cout << "Hello World!\n";
+    cout << "input\n";
     string c;
     cin >> c;
     cout << c;
